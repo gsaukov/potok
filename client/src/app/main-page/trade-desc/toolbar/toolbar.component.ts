f93ead06865b5ca31a60580ea9787c4df8a34675
Georgy Saukov <georgy.saukov@sap.com>
2024-11-04 23:07:39 +0100

    form rework

--- client/src/app/main-page/trade-desc/toolbar/toolbar.component.ts
+++ client/src/app/main-page/trade-desc/toolbar/toolbar.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import {SocketService} from '../../../services/socket.service';
-import {QuoteRequest} from '../../../services/socket.schema';
+import {NewOrder, QuoteRequest} from '../../../services/socket.schema';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  tradeForm!: FormGroup;
 
   constructor(private socketService: SocketService) {
   }
 
-  subscribe(symbol: string) {
+  subscribe() {
     this.socketService.sendQuoteRequest(new QuoteRequest(symbol))
   }
 
   newOrder() {
-
+    this.socketService.sendNewOrder(new NewOrder(symbol, route, val, volume))
   }
 
   sendDisconnect() {
