9aecc68f34465b43de884a5fe3d4cb28300a7216
Georgy Saukov <georgy.saukov@sap.com>
2024-11-05 22:53:13 +0100

    enabled quotes on trade desc

--- client/src/app/main-page/trade-desc/trade-desc.component.ts
+++ client/src/app/main-page/trade-desc/trade-desc.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ToolbarComponent} from './toolbar/toolbar.component';
+import {QuotesComponent} from './quotes/quotes.component';
 
 @Component({
   selector: 'app-trade-desc',
   standalone: true,
   imports: [
-    ToolbarComponent
+    ToolbarComponent,
+    QuotesComponent
   ],
   templateUrl: './trade-desc.component.html',
   styleUrl: './trade-desc.component.scss'
