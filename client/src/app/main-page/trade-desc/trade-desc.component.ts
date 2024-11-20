d295d0791f7b9609592ef1c4855cf5f966e5dad6
Georgy Saukov <georgy.saukov@sap.com>
2024-11-20 17:54:21 +0100

    orders table position.

--- client/src/app/main-page/trade-desc/trade-desc.component.ts
+++ client/src/app/main-page/trade-desc/trade-desc.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {ToolbarComponent} from './toolbar/toolbar.component';
 import {QuotesComponent} from './quotes/quotes.component';
+import {OrdersComponent} from './orders/orders.component';
 
 @Component({
   selector: 'app-trade-desc',
   standalone: true,
   imports: [
     ToolbarComponent,
-    QuotesComponent
+    QuotesComponent,
+    OrdersComponent
   ],
   templateUrl: './trade-desc.component.html',
   styleUrl: './trade-desc.component.scss'
