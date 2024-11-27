2bf5b63044f715cf1dba5b23f28a200925477165
Georgy Saukov <georgy.saukov@sap.com>
2024-11-27 21:10:59 +0100

    positions fix.

--- client/src/app/main-page/trade-desc/positions/positions.component.ts
+++ client/src/app/main-page/trade-desc/positions/positions.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import {MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import {SocketService} from '../../../services/socket.service';
-import {Position} from '../../../services/socket.schema';
+import {Execution, OrderConfirmation, Position} from '../../../services/socket.schema';
 
 @Component({
   selector: 'app-positions',
@@ -13,16 +13,27 @@ import {Position} from '../../../services/socket.schema';
 })
 export class PositionsComponent {
 
-  dataSource:Position[]
+  positions: Position[] = []
+  dataSource = new MatTableDataSource<Position>();
   displayedColumns: string[] = ['UUID', 'Symbol', 'Route', 'Quantity', 'Wap'];
 
   constructor(private socketService:SocketService) {
-    this.dataSource = [
-      {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
-      {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
-      {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
-    ]
+    // this.positions = [
+    //   {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+    //   {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+    //   {uuid:'Some UUID', createdTimestamp: 'timstamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+    // ]
+
+    this.socketService.listenPositionNotification().subscribe(p => {
+      this.positions.push(p)
+      this.dataSource.data = this.positions
+    })
   }
+
+  // applyExecution(execution: Execution) {
+  //   const order:OrderConfirmation = this.getOrderById(execution.orderUuid);
+  //   order.volume = execution.orderLeftQuantity;
+  // }
 
   isActive(position:Position) {
     return false;
