56ffd0330ef8addd3e403451ec459460b1ced5e5
Georgy Saukov <georgy.saukov@sap.com>
2024-12-03 22:41:00 +0100

    pagination attempt

--- client/src/app/main-page/trade-desc/orders/orders.component.ts
+++ client/src/app/main-page/trade-desc/orders/orders.component.ts
@@ -1,13 +1,15 @@
 import {ChangeDetectorRef, Component } from '@angular/core';
-import {CancelOrder, Execution, OrderConfirmation} from '../../../services/socket.schema';
-import {MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {CancelOrder, OrderConfirmation} from '../../../services/socket.schema';
+import {MatTableDataSource, MatTableModule,  } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import {SocketService} from '../../../services/socket.service';
+import {TEST_ORDERS} from './test.orders';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule],
+  imports: [MatTableModule, MatButtonModule, MatPaginator],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
@@ -17,22 +19,8 @@ export class OrdersComponent {
   displayedColumns: string[] = ['UUID', 'Symbol', 'Route', 'Price', 'Quantity', 'Filled', 'Left', 'Actions'];
 
   constructor(private socketService: SocketService, private changeDetector: ChangeDetectorRef) {
-
-    // this.dataSource = [
-    //   {symbol: 'SAPJ', route: 'BUY', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SELL', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SHORT', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SELL', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'BUY', uuid: 'Some UUID', val: 27, volume: 60, active: false, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SELL', uuid: 'Some UUID', val: 27, volume: 60, active: false, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SHORT', uuid: 'Some UUID', val: 27, volume: 60, active: false, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'BUY', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'BUY', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SELL', uuid: 'Some UUID', val: 27, volume: 60, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SELL', uuid: 'Some UUID', val: 27, volume: 0, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'BUY', uuid: 'Some UUID', val: 27, volume: 0, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    //   {symbol: 'SAPJ', route: 'SHORT', uuid: 'Some UUID', val: 27, volume: 0, active: true, originalVolume: 70, account: 'some acc', blockedPrice: -1, timestamp: "123123434324"},
-    // ]
+    this.orders = TEST_ORDERS
+    this.dataSource.data = this.orders
     this.socketService.listenOrderConfirm().subscribe(o => {
         this.orders.push(o)
         this.dataSource.data = this.orders
