5ebffe874f3226701edbfc84209a95544994573a
Georgy Saukov <georgy.saukov@sap.com>
2024-12-03 21:51:08 +0100

    test positions

---
+++ client/src/app/main-page/trade-desc/positions/test.positions.ts
@@ -0,0 +1,7 @@
+import {Position} from '../../../services/socket.schema';
+
+export const TEST_POSITIONS: Position[] = [
+  {uuid:'Some UUID', createdTimestamp: 'timestamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+  {uuid:'Some UUID', createdTimestamp: 'timsetamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+  {uuid:'Some UUID', createdTimestamp: 'timestamp', symbol: 'SAPJ', route: 'BUY', account: 'My Account', volume: 100, weightedAveragePrice: 99, averagePerformance: 0},
+]
