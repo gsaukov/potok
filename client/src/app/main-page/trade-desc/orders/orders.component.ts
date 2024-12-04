f9fe9f6e2a62f8be1f8f587dfd9d166a906f59b7
Georgy Saukov <georgy.saukov@sap.com>
2024-12-04 21:42:13 +0100

    orders pagination

--- client/src/app/main-page/trade-desc/orders/orders.component.ts
+++ client/src/app/main-page/trade-desc/orders/orders.component.ts
@@ -1,15 +1,16 @@
 import {ChangeDetectorRef, Component } from '@angular/core';
 import {CancelOrder, OrderConfirmation} from '../../../services/socket.schema';
 import {MatTableDataSource, MatTableModule,  } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import {SocketService} from '../../../services/socket.service';
 import {TEST_ORDERS} from './test.orders';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, MatPaginator],
+  imports: [SlicePipe, MatTableModule, MatButtonModule, MatPaginator],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss'
 })
@@ -17,6 +18,8 @@ export class OrdersComponent {
   orders: OrderConfirmation[] = []
   dataSource = new MatTableDataSource<OrderConfirmation>();
   displayedColumns: string[] = ['UUID', 'Symbol', 'Route', 'Price', 'Quantity', 'Filled', 'Left', 'Actions'];
+  lowValue = 0;
+  highValue = 5;
 
   constructor(private socketService: SocketService, private changeDetector: ChangeDetectorRef) {
     this.orders = TEST_ORDERS
@@ -45,6 +48,12 @@ export class OrdersComponent {
 
   getOrderById(uuid:string):OrderConfirmation {
     return this.orders.find(e => e.uuid === uuid)!
+  }
+
+  public getPaginatorData(event: PageEvent): PageEvent {
+    this.lowValue = event.pageIndex * event.pageSize;
+    this.highValue = this.lowValue + event.pageSize;
+    return event;
   }
 
 }
