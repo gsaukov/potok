44c405a1b5c9d7102c571f9a68a9cf8d851cc0b3
Georgy Saukov <georgy.saukov@sap.com>
2024-12-15 20:42:22 +0100

    eureka discovery removed.

--- depositary/src/main/java/com/apps/depositary/DepositaryApplication.java
+++ depositary/src/main/java/com/apps/depositary/DepositaryApplication.java
@@ -3,10 +3,8 @@ package com.apps.depositary;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class DepositaryApplication {
 
