1caccbe84909a0f6c9e5cabfcdc15537b2295ccd
Georgy Saukov <georgy.saukov@sap.com>
2024-12-27 21:44:57 +0100

    account repository.

---
+++ depositary/src/main/java/com/apps/depositary/persistance/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.apps.depositary.persistance.repository;
+
+
+import com.apps.depositary.persistance.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
