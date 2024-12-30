b7d21ad9489dc4a0fa1e6c7bcb108805226bd07c
Georgy Saukov <georgy.saukov@sap.com>
2024-12-30 21:28:58 +0100

    Depository service V2

---
+++ depositary/src/main/java/com/apps/depositary/service/depositV2/DepositaryServiceV2.java
@@ -0,0 +1,19 @@
+package com.apps.depositary.service.depositV2;
+
+import com.apps.depositary.persistance.entity.Execution;
+import com.apps.depositary.service.DepositExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//This is queue based deposit creation service that relies on database lock mechanic.
+@Service
+public class DepositaryServiceV2 {
+
+    @Autowired
+    private DepositExecutorV2 depositExecutor;
+
+    public void processExecution(Execution execution) {
+        depositExecutor.executeDeposit(execution);
+    }
+
+}
