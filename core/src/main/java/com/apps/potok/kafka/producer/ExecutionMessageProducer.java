f9df8e66df84122d5dc4ed792a3981672b7f1cb2
Georgy Saukov <georgy.saukov@sap.com>
2025-01-09 19:54:09 +0100

    core partitioned writes.

--- core/src/main/java/com/apps/potok/kafka/producer/ExecutionMessageProducer.java
+++ core/src/main/java/com/apps/potok/kafka/producer/ExecutionMessageProducer.java
@@ -1,16 +1,22 @@
 package com.apps.potok.kafka.producer;
 
 import com.apps.potok.soketio.model.execution.Execution;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.Charset;
 import java.util.concurrent.CompletableFuture;
 
 @Component
 public class ExecutionMessageProducer {
+
+    private static final Charset CHARSET = Charset.forName("US-ASCII");
+    private static final int HASH_BITS = 32;
 
     @Value(value = "${kafka.topic.executions}")
     private String topicName;
@@ -33,6 +39,16 @@ public class ExecutionMessageProducer {
     }
 
     public void sendExecutionMessage(Execution message) {
-        executionKafkaTemplate.send(topicName, message.getUuid().toString(),  message);
+        int partitionNumber = getPartitionNumber(message);
+        executionKafkaTemplate.send(topicName, partitionNumber, message.getUuid().toString(),  message);
+    }
+
+    private int getPartitionNumber(Execution execution) {
+        HashCode hash = Hashing.goodFastHash(HASH_BITS).hashString(getConsistentHashSource(execution), CHARSET);
+        return Hashing.consistentHash(hash, 4);
+    }
+
+    private String getConsistentHashSource(Execution execution) {
+        return String.join("", execution.getAccountId(), execution.getSymbol(), execution.getRoute().toString());
     }
 }
